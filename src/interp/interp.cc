222788bc1e8a2f623adcc05fdc43ca7b6fe0cdb8
Alex Beregszaszi <alex@rtfs.hu>
2019-07-24 20:27:39 +0100

    Remove assertion in interpreter loop and place a default switch case instead

--- src/interp/interp.cc
+++ src/interp/interp.cc
@@ -1510,7 +1510,6 @@ Result Thread::Run(int num_instructions) {
   const uint8_t* pc = &istream[pc_];
   for (int i = 0; i < num_instructions; ++i) {
     Opcode opcode = ReadOpcode(&pc);
-    assert(!opcode.IsInvalid());
     switch (opcode) {
       case Opcode::Select: {
         uint32_t cond = Pop<uint32_t>();
@@ -3292,6 +3291,10 @@ Result Thread::Run(int num_instructions) {
       case Opcode::Rethrow:
       case Opcode::Throw:
       case Opcode::Try:
+        WABT_UNREACHABLE;
+        break;
+
+      default:
         WABT_UNREACHABLE;
         break;
     }
